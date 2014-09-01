7bad2c76fba00ae59720c15c9bcb4ee31975881a
Erik Carlsson <info@code301.com>
2014-09-01 22:35:10 +0200

    Init.

---
+++ lib/Dist/Zilla/PluginBundle/CSSON.pm
@@ -0,0 +1,39 @@
+package Dist::Zilla::PluginBundle::CSSON;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Dist::Zilla::PluginBundle::CSSON - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Dist::Zilla::PluginBundle::CSSON;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::PluginBundle::CSSON is
+
+=head1 AUTHOR
+
+Erik Carlsson E<lt>info@code301.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Erik Carlsson
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/basic.t
@@ -0,0 +1,8 @@
+use strict;
+use Test::More;
+use Dist::Zilla::PluginBundle::CSSON;
+
+# replace with the actual test
+ok 1;
+
+done_testing;
